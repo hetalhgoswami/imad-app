c48396074123a4c1ed5e638fde83c399cf171156
hetalhgoswami <30928199+hetalhgoswami@users.noreply.github.com>
2017-08-19 17:36:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = { 
-'articleone' : {
+var articleone : {
 title: 'Article One',
 heading: 'Article One',
 date: 'Aug 18 2017',
@@ -14,7 +14,7 @@ content: `<p>This is the paragraph tag.</p>
 <p>This is Another paragraph tag.</p>
 <p>This is One more paragraph tag.</p>`
 },
-'articletwo' : {
+var articletwo : {
 title: 'Article Two',
 heading: 'Article Two',
 date: 'Aug 20 2017',
@@ -22,7 +22,7 @@ content: `<p>This is the paragraph tag.</p>
 <p>This is Another paragraph tag.</p>
 <p>This is One more paragraph tag.</p>`
 },
-'articlethree' : {
+var articlethree : {
 title: 'Article Three',
 heading: 'Article Three',
 date: 'Aug 22 2017',
@@ -66,7 +66,7 @@ var htmltemplate =`
 
 `;
 return htmltemplate;
-}
+};
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
