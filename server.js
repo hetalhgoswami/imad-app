3b102835ac00826f05d4df838f58ae1ca21f2144
hetalhgoswami <30928199+hetalhgoswami@users.noreply.github.com>
2017-08-19 17:33:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,79 +5,84 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne= {
-    title: "Article one|hetal goswami",
-    headding: 'Article-one',
-    date: 'sep 5 2017',
-    content: `p>
-                This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code.
-            </p>
-            <p>
-                This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code.
-            </p>
-            <p>
-                This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code. This is my first html code.
-            </p>`
-        
+var articles = { 
+'articleone' : {
+title: 'Article One',
+heading: 'Article One',
+date: 'Aug 18 2017',
+content: `<p>This is the paragraph tag.</p>
+<p>This is Another paragraph tag.</p>
+<p>This is One more paragraph tag.</p>`
+},
+'articletwo' : {
+title: 'Article Two',
+heading: 'Article Two',
+date: 'Aug 20 2017',
+content: `<p>This is the paragraph tag.</p>
+<p>This is Another paragraph tag.</p>
+<p>This is One more paragraph tag.</p>`
+},
+'articlethree' : {
+title: 'Article Three',
+heading: 'Article Three',
+date: 'Aug 22 2017',
+content: `<p>This is the paragraph tag.</p>
+<p>This is Another paragraph tag.</p>
+<p>This is One more paragraph tag.</p>`
+}
 };
-function createTamplate(Data)
-{
-    var title = Data.title;
-    var date = Data.date;
-    var headding = Data.headding;
-    var content = Data.content;
-    var htmltamplate=
-   `<html>
-     <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width-device-width initial-scale-1"/>
-          <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-          ${headding}
-        </h3>
-        <div>
-            ${date}
-        </div>
-        <div>
-            ${content}
-        </div>
-        </div>
-    </body>
-</html>`;
+
+function createtemplate (data){
+
+var title = data.title;
+var heading = data.heading;
+var date = data.date;
+var content = data.content;
+
+var htmltemplate =`
+<html>
+<head>
+    <title>${title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+    <div> 
+        <a href="/">Home</a>
+    </div>
+    
+    <hr/>
+    
+    <h3>Article One</h3>
+    
+    <div>${date}</div>
+    
+    <div>
+        ${content}
+    </div>
+    </div>
+</body>
+
+`;
 return htmltemplate;
 }
-
-
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-one',function(req,res){
-   res.send(createTemplate(articleOne))
-});
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/:articleName',function(req, res){
+// articleName = article-one 
+// articles[articleName] == { content of the object of articles
+var articleName = req.params.articleName;
+res.send(createtemplate(articles[articleName]));
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
