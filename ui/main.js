3430ffe20316db39952097e36c4c2d78a2009ad1
hetalhgoswami <30928199+hetalhgoswami@users.noreply.github.com>
2017-08-23 13:50:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,23 @@
 console.log('Loaded!');
+alert ('modified main.js');
+var element = document.getElementById('main-text');
+element.innerHTML = "new text modified by main.js";
+var img = document.getElementById('madi');
+img.onclick=function() {
+img.style.marginLeft='100px';
+};
+//gradually move the image
+var marginLeft = 0;
+function moveRight() {
+marginLeft = marginLeft + 5;
+img.style.marginLeft= marginLeft + 'px';
+}
+img.onclick=function() {
+var interval = setInterval(moveRight,50);
+};
+
+
+/*console.log('Loaded!');
 // change the text of main-text div
 var element =document.getElementById("main-text");
 element.innerHTML='New value';
@@ -14,7 +33,7 @@ img.style.marginLeft = marginLeft + 'px';
 img.onClick = function(){
 var interval = setInterval(moveRight, 100);
 
-};*/
+};
 var marginLeft=0;
 function moveRight()
 {
@@ -24,4 +43,4 @@ function moveRight()
 img.onclick=function(){
     var interval=setInterval(moveRight,70);
     img.style.marginLeft='100px';
-};
+};*/
