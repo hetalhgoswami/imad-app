e92d1dca749b45396f9482199c8faae067830aff
hetalhgoswami <30928199+hetalhgoswami@users.noreply.github.com>
2017-08-23 13:46:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML='New value';
 var img= document.getElementById('madi');
 var img = document.getElementById("image");
 
-var marginLeft = 0;
+/*var marginLeft = 0;
 function moveRight () {
 marginLeft = marginLeft + 10;
 img.style.marginLeft = marginLeft + 'px';
@@ -15,7 +15,7 @@ img.onClick = function(){
 var interval = setInterval(moveRight, 100);
 
 };
-/*var marginLeft='0';
+var marginLeft='0';
 function moveRight()
 {
     marginLeft=marginLeft+10;
