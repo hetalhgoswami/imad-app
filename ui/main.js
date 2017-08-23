c8d17aec772a1eecd64fc12d45ade964578fdaec
hetalhgoswami <30928199+hetalhgoswami@users.noreply.github.com>
2017-08-23 13:23:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ console.log('Loaded!');
 var element =document.getElementById("main-text");
 element.innerHTML='New value';
 var img= document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft+'px';
+}
 img.onclick=function(){
+    var interval=setInterval(moveRight,70);
     img.style.marginLeft='100px';
 };
