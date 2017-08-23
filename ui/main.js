e9dcff773699662703b76a8e70b6555331ca2f27
hetalhgoswami <30928199+hetalhgoswami@users.noreply.github.com>
2017-08-23 13:54:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,40 +7,25 @@ img.onclick=function() {
 img.style.marginLeft='100px';
 };
 //gradually move the image
-var marginLeft = 0;
+/*var marginLeft = 0;
 function moveRight() {
 marginLeft = marginLeft + 5;
 img.style.marginLeft= marginLeft + 'px';
 }
 img.onclick=function() {
 var interval = setInterval(moveRight,50);
-};
+};*/
 
 
-/*console.log('Loaded!');
-// change the text of main-text div
-var element =document.getElementById("main-text");
-element.innerHTML='New value';
-var img= document.getElementById('madi');
-var img = document.getElementById("image");
 
-/*var marginLeft = 0;
-function moveRight () {
-marginLeft = marginLeft + 10;
-img.style.marginLeft = marginLeft + 'px';
-}
 
-img.onClick = function(){
-var interval = setInterval(moveRight, 100);
-
-};
-var marginLeft=0;
+marginLeft=0;
 function moveRight()
 {
     marginLeft=marginLeft+10;
-    img.style.marginLeft+'px';
+    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
-    var interval=setInterval(moveRight,70);
-    img.style.marginLeft='100px';
-};*/
+    var interval=setInterval(moveRight+70);
+    
+};
