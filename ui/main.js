7abc698c427ee06e16c27faaa7049ea0d692509f
hetalhgoswami <30928199+hetalhgoswami@users.noreply.github.com>
2017-08-31 14:03:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,59 @@
-console.log('Loaded!');
+//couter code
+var button =document.getElementById('couter');
+var couter=0;
+button.onclick=function(){
+    //render the variable in coreect span
+    coutr=coutr+1;
+    var span=document.getElementById('couter');
+    span.innerHTML=couter.toString();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*console.log('Loaded!');
 alert ('modified main.js');
 var element = document.getElementById('main-text');
 element.innerHTML = "new text modified by main.js";
@@ -19,7 +74,7 @@ var interval = setInterval(moveRight,50);
 
 
 
-/*marginLeft=0;
+marginLeft=0;
 function moveRight()
 {
     marginLeft=marginLeft+10;
